0552eb22e8c97a2a90579d4efa3855e48e95d874
Zoe Farrell <zoe@turing.edu>
2023-04-05 15:56:40 -0600

    Add Trail Controller

---
+++ GreenMountain/Controllers/TrailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenMountain.Controllers
+{
+    public class TrailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
