ffd68df72d12d56c2354930817af0891509eca56
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2023-04-05 14:56:24 -0600

    Add tables to dbcontext

--- GreenMountain/DataAccess/GreenMountainContext.cs
+++ GreenMountain/DataAccess/GreenMountainContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GreenMountain.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GreenMountain.DataAccess
 {
     public class GreenMountainContext : DbContext 
     {
+        public DbSet<Trail> Trails { get; set; }
+
         public GreenMountainContext(DbContextOptions<GreenMountainContext> options) : base(options)
         {
 
