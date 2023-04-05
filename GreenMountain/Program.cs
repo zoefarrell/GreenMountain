8c721afbf161ac9426a974cc11b13adc642e511d
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2023-04-05 15:15:20 -0600

    Add Notes

--- GreenMountain/Program.cs
+++ GreenMountain/Program.cs
@@ -6,6 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//MM NOTE: getting a warning about not needing the AddEntityFramework call - still need to play around with that.
+//MM NOTE: used the wrong case convention, now everything is ALL CAPS :(
+//MM NOTE: I'm wondering if we need the EFCore.Design package - need to look into what its doing with Migrations
+
 builder.Services.AddEntityFrameworkNpgsql()
     .AddDbContext<GreenMountainContext>(opt => 
     opt.UseNpgsql(builder.Configuration.GetConnectionString("GreenMountainDb")).UseUpperSnakeCaseNamingConvention());
