5d0b6f039dba759476d420fafcf828efa44b692e
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2023-04-05 14:15:05 -0600

    Add Trail Model

---
+++ GreenMountain/Models/Trail.cs
@@ -0,0 +1,9 @@
+﻿namespace GreenMountain.Models
+{
+    public class Trail
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Rating { get; set; }
+    }
+}
