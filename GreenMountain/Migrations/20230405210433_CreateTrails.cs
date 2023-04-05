5681c01ec2eb58a2fde7d31eb6f202b3f8f018df
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2023-04-05 15:14:29 -0600

    Add table migrations

---
+++ GreenMountain/Migrations/20230405210433_CreateTrails.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenMountain.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTrails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TRAILS",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "text", nullable: false),
+                    NAME = table.Column<string>(type: "text", nullable: false),
+                    RATING = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TRAILS", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TRAILS");
+        }
+    }
+}
